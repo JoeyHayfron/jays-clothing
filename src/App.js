6b45a45cf9e8e00ece5ca7823433e460077815a5
Joeyy Hayfron <joeacq81@gmail.com>
2020-07-30 00:28:28 +0000

    Completed Shops Page and added styling to it

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.components";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }

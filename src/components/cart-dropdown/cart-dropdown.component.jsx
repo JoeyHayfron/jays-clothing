f376837c6fdc730c8a4cde623a1cd6f8d9d7880c
Joeyy Hayfron <joeacq81@gmail.com>
2020-08-12 10:59:04 +0000

    Updated cart icon and cart dropdown using slector and reselct library

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = () => (
+const CartDropDown = ({ items }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {items.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = (state) => ({
+  items: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropDown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,17 +3,21 @@ import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 import { toggleCartDropDown } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.style.scss';
 
-const CartIcon = ({ toggleCartDropDown }) => (
+const CartIcon = ({ toggleCartDropDown, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartDropDown}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 const mapDispatchToProps = (dispatch) => ({
   toggleCartDropDown: () => dispatch(toggleCartDropDown()),
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
